b4cea65b5942ab4910f2b6bd29920f782194460a
Jon <68740476+bytejon@users.noreply.github.com>
2022-10-09 13:26:41 -0400

    abc

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+const base_url = "http://127.0.0.1:5500/";
+
+async function get_file(url) {
+	let response = await fetch(url);
+	let text = await response.text();
+
+	console.log(text);
+}
+
+console.log("RUNNING JS");
+console.log(window.location.href);
+
+// get_file(base_url + "assets/articles/test_1");
+
+// function find_parent_of_class(node, name) {
+// 	let node = node;
+// 	while (node !== document.body) {
+// 		if (node.className === name) return node;
+// 		node = node.parentElement;
+// 	}
+// 	return false;
+// }
+
+// let list_items = document.getElementsByClassName("list-item");
+
+// function on_click(event) {
+// 	console.log(event.target.closest("div"));
+// }
+
+// for (object of list_items) {
+// 	object.addEventListener("click", on_click, false);
+// }
